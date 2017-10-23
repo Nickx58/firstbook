26049a16027de063e8544cb69e8aadaa9255607a
Nikhil Sharma <nikhilx58@gmail.com>
2017-10-23 08:08:13 +0530

    creating google credentials

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://nick:5855@ds227525.mlab.com:27525/firstbooks-dev',
+    googleClientID:'660368003540-qajjql7j4or3n9ilk5v4cu65c7um73o7.apps.googleusercontent.com',
+    googleClientSecret:'JZZgd8mC6paLAcxspk-ycQGE'
+}
