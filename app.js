16eab09e0342e553ee851b2d655f44df51c5a80a
Nikhil Sharma <nikhilx58@gmail.com>
2017-10-24 05:52:06 +0530

    Index route and handlebar

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-// load User Model
+// Load User Model
 require('./models/User');
 
-// passport config
+// Passport Config
 require('./config/passport')(passport);
 
-// load routes
+// Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
-// load keys
+// Load Keys
 const keys = require('./config/keys');
 
-// Map global promise
+// Map global promises
 mongoose.Promise = global.Promise;
-
 // Mongoose Connect
-mongoose.connect(keys.mongoURI,{
-    useMongoClient : true
-}).then(() => console.log('Mongo DB connected')).catch(err => console.log(err));
+mongoose.connect(keys.mongoURI, {
+  useMongoClient:true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('It works');
-});
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 app.use(session({
-    secret : 'secret',
-    resave : false,
-    saveUninitialized : false
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
 }));
 
-// passport middleware
-
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// set global var
-app.use((req,res,next) => {
-    res.locals.user = req.user || null;
-    next();
-})
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
 
-// Use route
-app.use('/auth',auth);
+// Use Routes
+app.use('/', index);
+app.use('/auth', auth);
 
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,() => {
-    console.log(`Server is started on Port ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
